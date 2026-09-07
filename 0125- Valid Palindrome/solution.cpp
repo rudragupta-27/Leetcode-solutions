59b0ae26a4a8e19afd9bf568838dd5e203096238
Rudra-Gupta27 <rudrguptarl@gmail.com>
2026-09-07 22:15:44 +0530

    Adding Leetcode solutions

---
+++ 0125- Valid Palindrome/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            if (tolower(s[left]) == tolower(s[right])) {
+                left++;
+                right--;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
